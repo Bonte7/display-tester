ec65918c708fc90a9d1dda4f91d7f7039ba2672c
Christian Bonte <cjbonte7@gmail.com>
2023-03-27 17:11:51 -0400

    CSS updates

--- src/Display.js
+++ src/Display.js
@@ -5,7 +5,7 @@ function Display(props) {
     return (
         <div className="display-container">
             <h1>Results</h1>
-            <iframe src={`https://ui.powerreviews.com/stable/4.1/standalone/${props.standaloneType}/index.html?pr_merchant_id=${props.merchantId}&pr_merchant_group_id=${props.merchantGroupId}&pr_page_id=${props.pageId}&pr_api_key=${props.apiKey}&${props.simpleSnap}&${props.verticalPag}&${props.condensed}&${props.reviewFilter}`} title="pwr" width="1500px" height="1000px"></iframe>
+            <iframe className="pwr-display" src={`https://ui.powerreviews.com/stable/4.1/standalone/${props.standaloneType}/index.html?pr_merchant_id=${props.merchantId}&pr_merchant_group_id=${props.merchantGroupId}&pr_page_id=${props.pageId}&pr_api_key=${props.apiKey}&${props.simpleSnap}&${props.verticalPag}&${props.condensed}&${props.reviewFilter}`} title="pwr"></iframe>
         </div>
     );
 }
--- src/Form.js
+++ src/Form.js
@@ -6,52 +6,56 @@ const Form = ({handleSubmitEvent, handleChangeEvent, handleCheckbox, chosenDispl
 
     return (
         <div className="form-container">
-        <h1 className="form">Input Form</h1>
+        <h1 className="form">Standalone Generator</h1>
         <form onSubmit={handleSubmitEvent}>
-            <label>
-                Merchant Group ID:
-                <input type="number" name="merchantGroupId" onChange={handleChangeEvent}></input>
-            </label>
-            <label>
-                Merchant ID:
-                <input type="number" name="merchantId" onChange={handleChangeEvent}></input>
-            </label>
-            <label>
-                API Key:
-                <input type="text" name="apiKey" onChange={handleChangeEvent}></input>
-            </label>
-            <label>
-                Page ID:
-                <input type="text" name="pageId" onChange={handleChangeEvent}></input>
-            </label>
-            <label>
-                Display Type:
-                <select name="displayType" onChange={handleChangeEvent}>
-                    <option>-</option>
-                    <option value="display">Display</option>
-                    <option value="collect">WAR</option>
-                </select>
-            </label>
-            {chosenDisplay === "display" ? <div className="additional-display-fields">
+            <div className="cred-fields">
+                <label>
+                    Merchant Group ID:
+                    <input type="number" name="merchantGroupId" onChange={handleChangeEvent}></input>
+                </label>
+                <label>
+                    Merchant ID:
+                    <input type="number" name="merchantId" onChange={handleChangeEvent}></input>
+                </label>
+                <label>
+                    API Key:
+                    <input type="text" name="apiKey" onChange={handleChangeEvent}></input>
+                </label>
+                <label>
+                    Page ID:
+                    <input type="text" name="pageId" placeholder="(Not Required for WAR)" onChange={handleChangeEvent}></input>
+                </label>
+                <label>
+                    Display Type:
+                    <select name="displayType" onChange={handleChangeEvent}>
+                        <option>-</option>
+                        <option value="display">Display</option>
+                        <option value="collect">WAR</option>
+                    </select>
+                </label>
+            </div>
+            {chosenDisplay === "display" ? <div className="additional-display-container">
                 <h3>Display Configurations:</h3>
+                <div className="display-options">
                 <label>
-                    Simple Display
                     <input type="checkbox" name="simpleSnap" value="REVIEW_DISPLAY_SNAPSHOT_TYPE=SIMPLE" onChange={handleCheckbox}></input>
+                    Simple Display
                 </label>
                 <label>
-                    Vertical Pagination
                     <input type="checkbox" name="verticalPagination" value="REVIEW_DISPLAY_PAGINATION_TYPE=VERTICAL" onChange={handleCheckbox}></input>
+                    Vertical Pagination
                 </label>
                 <label>
-                    Condensed List
                     <input type="checkbox" name="condensed" value="REVIEW_DISPLAY_LIST_TYPE=CONDENSED" onChange={handleCheckbox}></input>
+                    Condensed List
                 </label>
                 <label>
-                    Review Filtering
                     <input type="checkbox" name="reviewFilter" value="ENABLE_REVIEW_FILTERING=1" onChange={handleCheckbox}></input>
+                    Review Filtering
                 </label>
+                </div>
             </div> : null}
-            <input type="submit"></input>
+            <input className="submit-button" type="submit"></input>
         </form>
       </div>
     );
