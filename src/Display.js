68b2d27897a7c07d512466ae7edbc1c6121443fe
Christian Bonte <cjbonte7@gmail.com>
2023-03-29 11:20:55 -0400

    minor changes to results url

--- src/Display.js
+++ src/Display.js
@@ -4,7 +4,7 @@ function Display(props) {
 
     return (
         <div className="display-container">
-            <h1>Results</h1>
+            <p className="results-url">Showing Results for: {`https://ui.powerreviews.com/stable/4.1/standalone/${props.standaloneType}/index.html?pr_merchant_id=${props.merchantId}&pr_merchant_group_id=${props.merchantGroupId}&pr_page_id=${props.pageId}&pr_api_key=${props.apiKey}&${props.simpleSnap}&${props.verticalPag}&${props.condensed}&${props.reviewFilter}`}</p>
             <iframe className="pwr-display" src={`https://ui.powerreviews.com/stable/4.1/standalone/${props.standaloneType}/index.html?pr_merchant_id=${props.merchantId}&pr_merchant_group_id=${props.merchantGroupId}&pr_page_id=${props.pageId}&pr_api_key=${props.apiKey}&${props.simpleSnap}&${props.verticalPag}&${props.condensed}&${props.reviewFilter}`} title="pwr"></iframe>
         </div>
     );
